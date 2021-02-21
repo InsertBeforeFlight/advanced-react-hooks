67bfa11661f7b9caa7883a91b323c25b41249ff7
InsertBeforeFlight <joshuacrowe@outlook.com>
2021-02-21 20:33:05 +0000

    Exercise 1 Extra Credit 4

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,17 +3,21 @@
 
 import { useReducer } from 'react'
 
+const INCREMENT_ACTION_TYPE = "INCREMENT";
+
 function countReducer(state, action) {
-  return {
-    ...state,
-    ...(typeof action === 'function') ? action(state) : action
-  };
+  switch (action.type) {
+    case INCREMENT_ACTION_TYPE:
+      return { count: state.count + action.step }
+    default:
+      throw new Error(`Unsupported Action Type: ${action.type}`)
+  }
 }
 
 function Counter({initialCount = 0, step = 1}) {
   const [state, setState] = useReducer(countReducer, { count: initialCount })
 
-  const increment = () => setState(currentState => ({ count: currentState.count + step }))
+  const increment = () => setState({ type: INCREMENT_ACTION_TYPE, step });
   return <button onClick={increment}>{state.count}</button>
 }
 
