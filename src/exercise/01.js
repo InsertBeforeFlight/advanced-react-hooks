7d98b7d123f27bff0005cb89ebdb1ea761cc34c1
InsertBeforeFlight <joshuacrowe@outlook.com>
2021-02-21 20:25:18 +0000

    Exercise 1 Extra Credit 3

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,14 +3,17 @@
 
 import { useReducer } from 'react'
 
-function countReducer({ count }, { step }) {
-  return { count: count + step }
+function countReducer(state, action) {
+  return {
+    ...state,
+    ...(typeof action === 'function') ? action(state) : action
+  };
 }
 
 function Counter({initialCount = 0, step = 1}) {
   const [state, setState] = useReducer(countReducer, { count: initialCount })
 
-  const increment = () => setState({ step })
+  const increment = () => setState(currentState => ({ count: currentState.count + step }))
   return <button onClick={increment}>{state.count}</button>
 }
 
